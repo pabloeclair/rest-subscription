41f2498147be7bbb33bde0c88a24c017c1834730
pabloeclair <ergle.pasha@gmail.com>
2025-07-19 13:15:57 +0300

    added exseptionDto and logger struct

--- internal/sbscrb/model.go
+++ internal/sbscrb/model.go
@@ -1,12 +1,14 @@
-package sbscr
+package sbscrb
 
 import (
+	"encoding/json"
+	"net/http"
 	"time"
 
 	"gorm.io/gorm"
 )
 
-type Subscribe struct {
+type SubscribeDto struct {
 	gorm.Model
 	Id          uint      `gorm:"primaryKey" json:"id"`
 	ServiceName string    `gorm:"size:255;not null;default:null" json:"service_name"`
@@ -14,4 +16,36 @@ type Subscribe struct {
 	UserId      string    `gorm:"not null;default:null" json:"user_id"`
 	StartDate   time.Time `gorm:"not null;default:null" json:"start_date"`
 	EndDate     time.Time `json:"end_date"`
+}
+
+type ExceptionDto struct {
+	StatusCode   int    `json:"status_code"`
+	ErrorMessage string `json:"error_message"`
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	StatusCode    int
+	StatusMessage string
+}
+
+func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK, "OK"}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.StatusCode = code
+	lrw.StatusMessage = http.StatusText(code)
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+func (lrw *loggingResponseWriter) Write(b []byte) (int, error) {
+	var errDto ExceptionDto
+	if err := json.Unmarshal(b, &errDto); err != nil {
+		return 0, err
+	}
+	if errDto.ErrorMessage != "" {
+		lrw.StatusMessage += ": " + errDto.ErrorMessage
+	}
+	return lrw.ResponseWriter.Write(b)
 }
