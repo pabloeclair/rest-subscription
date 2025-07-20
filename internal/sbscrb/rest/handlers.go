22f77dede7d0fbe322e6d402941dad39b477cb9d
Pablo_3_16 <ergle.pasha@gmail.com>
2025-07-20 12:27:22 +0300

    implemented create handler

--- internal/sbscrb/rest/handlers.go
+++ internal/sbscrb/rest/handlers.go
@@ -1,1 +1,56 @@
 package rest
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/pabloeclair/rest-subscription/internal/sbscrb/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DSN string
+
+func Create(w http.ResponseWriter, r *http.Request) {
+
+	var (
+		subscribeDto models.SubscribeDto
+		errDto       models.FullExceptionDto
+	)
+
+	d := json.NewDecoder(r.Body)
+	if err := d.Decode(&subscribeDto); err != nil {
+		errDto = models.NewFullExceptionDto(
+			http.StatusBadRequest,
+			"Incorrect JSON body",
+			err.Error(),
+		)
+		errDto.Write(w)
+		return
+	}
+
+	db, err := gorm.Open(postgres.Open(DSN), &gorm.Config{Logger: logger.Default})
+	if err != nil {
+		errDto = models.NewFullExceptionDto(
+			http.StatusInternalServerError,
+			"Failed to connect to the database",
+			err.Error(),
+		)
+		errDto.Write(w)
+		return
+	}
+
+	res := db.Create(&subscribeDto)
+	if res.Error != nil {
+		errDto = models.NewFullExceptionDto(
+			http.StatusInternalServerError,
+			"Failed to create subscribe",
+			res.Error.Error(),
+		)
+		errDto.Write(w)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
