7df7400e82aa5e99522dde843ed831f312c90aac
Pablo_3_16 <ergle.pasha@gmail.com>
2025-07-21 19:57:01 +0300

    added update func

--- internal/sbscrb/rest/handlers.go
+++ internal/sbscrb/rest/handlers.go
@@ -189,6 +189,92 @@ func List(w http.ResponseWriter, r *http.Request) {
 	w.Write(b)
 }
 
+func Update(w http.ResponseWriter, r *http.Request) {
+
+	var (
+		subscribeDb  models.SubscribeDto
+		subscribeDto models.SubscribeDto
+		errDto       models.FullExceptionDto
+	)
+
+	idStr := r.PathValue("id")
+	idInt, err := strconv.Atoi(idStr)
+	if err != nil {
+		errDto = models.NewFullExceptionDto(
+			http.StatusBadRequest,
+			"Incorrect id in the URL path. Please specify a positive number",
+			err.Error(),
+		)
+		errDto.Write(w)
+		return
+	}
+
+	d := json.NewDecoder(r.Body)
+	if err := d.Decode(&subscribeDto); err != nil {
+		errDto = models.NewFullExceptionDto(
+			http.StatusBadRequest,
+			"Incorrect JSON body",
+			err.Error(),
+		)
+		errDto.Write(w)
+		return
+	}
+
+	db, err := connectToDB(w)
+	if err != nil {
+		return
+	}
+
+	res := db.First(&subscribeDb, idInt)
+	if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+		errDto = models.NewFullExceptionDto(
+			http.StatusNotFound,
+			fmt.Sprintf("The subscribe with id = %d is not found", idInt),
+			"",
+		)
+		errDto.Write(w)
+		return
+	} else if res.Error != nil {
+		errDto = models.NewFullExceptionDto(
+			http.StatusInternalServerError,
+			fmt.Sprintf("Failed to get the subscribe with id = %d", idInt),
+			res.Error.Error(),
+		)
+		errDto.Write(w)
+		return
+	}
+
+	if subscribeDto.ServiceName == "" {
+		subscribeDto.ServiceName = subscribeDb.ServiceName
+	}
+	if subscribeDto.Price == 0 {
+		subscribeDto.Price = subscribeDb.Price
+	}
+	if subscribeDto.UserId == "" {
+		subscribeDto.UserId = subscribeDb.UserId
+	}
+	if subscribeDto.StartDate.IsZero() {
+		subscribeDto.StartDate = subscribeDb.StartDate
+	}
+	if subscribeDto.EndDate == nil {
+		subscribeDto.EndDate = subscribeDb.EndDate
+	}
+
+	res = db.Model(&subscribeDb).Updates(&subscribeDto)
+	if res.Error != nil {
+		errDto = models.NewFullExceptionDto(
+			http.StatusInternalServerError,
+			fmt.Sprintf("Failed to update the subscribe with id = %d", idInt),
+			res.Error.Error(),
+		)
+		errDto.Write(w)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+
+}
+
 func Delete(w http.ResponseWriter, r *http.Request) {
 
 	var (
