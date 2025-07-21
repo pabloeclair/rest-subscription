1e2b9a37866f8a881125526625853b31e48fd9ce
Pablo_3_16 <ergle.pasha@gmail.com>
2025-07-21 19:55:43 +0300

    changed EndDate to pointer

--- internal/sbscrb/models/subcribe.go
+++ internal/sbscrb/models/subcribe.go
@@ -8,10 +8,10 @@ import (
 
 type SubscribeDto struct {
 	gorm.Model
-	Id          uint      `gorm:"primaryKey" json:"id"`
-	ServiceName string    `gorm:"size:255;not null;default:null" json:"service_name"`
-	Price       int       `gorm:"not null;default:null" json:"price"`
-	UserId      string    `gorm:"not null;default:null" json:"user_id"`
-	StartDate   time.Time `gorm:"not null;default:null" json:"start_date"`
-	EndDate     time.Time `json:"end_date"`
+	Id          uint       `gorm:"primaryKey" json:"id"`
+	ServiceName string     `gorm:"size:255;not null;default:null" json:"service_name"`
+	Price       int        `gorm:"not null;default:null" json:"price"`
+	UserId      string     `gorm:"not null;default:null" json:"user_id"`
+	StartDate   time.Time  `gorm:"not null;default:null" json:"start_date"`
+	EndDate     *time.Time `json:"end_date"`
 }
