a4b87e87bb5db1295f3448fe3da1dc85cd0733c5
Pablo_3_16 <ergle.pasha@gmail.com>
2025-07-24 17:49:12 +0300

    fix: change Price field to a pointer

--- internal/sbscrb/models/subcribe.go
+++ internal/sbscrb/models/subcribe.go
@@ -10,7 +10,7 @@ type SubscribeDto struct {
 	gorm.Model
 	Id          uint       `gorm:"primaryKey" json:"id"`
 	ServiceName string     `gorm:"size:255;not null;default:null" json:"service_name"`
-	Price       int        `gorm:"not null;default:null" json:"price"`
+	Price       *int       `gorm:"not null;default:null" json:"price"`
 	UserId      string     `gorm:"not null;default:null" json:"user_id"`
 	StartDate   time.Time  `gorm:"not null;default:null" json:"start_date"`
 	EndDate     *time.Time `json:"end_date"`
