188841fefa74d765232a273572ea74db2ed7f301
Pablo_3_16 <ergle.pasha@gmail.com>
2025-07-27 00:30:16 +0300

    fix: moved init func to init_server.go and updated mux

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 	"time"
 
@@ -20,76 +17,10 @@ import (
 	"gorm.io/gorm"
 )
 
-var (
-	shutdownDuration time.Duration
-	postgresTimeout  time.Duration
-	serverAddr       string
-	isSuccessfulInit bool = false
-)
-
-func init() {
-	var errs error
-	if len(os.Args) != 2 {
-		errs = errors.New("ERROR: please provide only one argument - server address (e.g. 'localhost:8080')")
-	}
-
-	shutdownDurationString := os.Getenv("SHUTDOWN_DURATION")
-	shutdownDurationInt, err := strconv.Atoi(shutdownDurationString)
-	if err != nil || shutdownDurationInt <= 0 {
-		errs = errors.Join(errs, errors.New("ERROR: the environment variable 'SHUTDOWN_DURATION' must be a positive number"))
-	} else {
-		shutdownDuration = time.Duration(shutdownDurationInt) * time.Second
-	}
-
-	postgresUser := os.Getenv("POSTGRES_USER")
-	if postgresUser == "" {
-		errs = errors.Join(errs, errors.New("ERROR: the environment variable 'POSTGRES_USER' is not found"))
-	}
-
-	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
-	if postgresPassword == "" {
-		errs = errors.Join(errs, errors.New("ERROR: the environment variable 'POSTGRES_PASSWORD' is not found"))
-	}
-
-	postgresDatabase := os.Getenv("POSTGRES_DB")
-	if postgresDatabase == "" {
-		errs = errors.Join(errs, errors.New("ERROR: the environment variable 'POSTGRES_DB' is not found"))
-	}
-
-	postgresHost := os.Getenv("POSTGRES_HOST")
-	if postgresHost == "" {
-		errs = errors.Join(errs, errors.New("ERROR: the environment variable 'POSTGRES_HOST' is not found"))
-	}
-
-	postgresTimeoutString := os.Getenv("POSTGRES_TIMEOUT")
-	postgresTimeoutInt, err := strconv.Atoi(postgresTimeoutString)
-	if err != nil || postgresTimeoutInt <= 0 {
-		errs = errors.Join(errs, errors.New("ERROR: the environment variable 'POSTGRES_TIMEOUT' must be a positive number"))
-	} else {
-		postgresTimeout = time.Duration(postgresTimeoutInt) * time.Second
-	}
-
-	models.NotificationInternalError = os.Getenv("NOTIFICATION_INTERNAL_ERROR")
-	if models.NotificationInternalError == "" {
-		color.Yellow("WARN: the environment variable 'NOTIFICATION_INTERNAL_ERROR' is not found. " +
-			"This variable is optional, but you may want to send an extra notification when catching INTERNAL SERVER ERROR " +
-			"(e.g., 'Please notify the administrator.')")
-	}
-
-	if errs != nil {
-		color.Red(errs.Error())
-		return
-	} else {
-		isSuccessfulInit = true
-		serverAddr = os.Args[1]
-		rest.DSN = fmt.Sprintf("host=%s port=5432 user=%s dbname=%s password=%s sslmode=disable",
-			postgresHost, postgresUser, postgresDatabase, postgresPassword)
-	}
-}
-
 func main() {
-
-	if !isSuccessfulInit {
+	shutdownDuration, postgresTimeout, serverAddrs, err := rest.Init()
+	if err != nil {
+		color.Red(err.Error())
 		return
 	}
 
@@ -117,10 +48,10 @@ func main() {
 	// starting server
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /api/v1/subscribes", rest.Create)
-	mux.HandleFunc("GET /api/v1/subscribes/{id}", rest.Get)
-	mux.HandleFunc("GET /api/v1/subscribe", rest.List)
-	mux.HandleFunc("PUT /api/v1/subscribes/{id}", rest.Update)
-	mux.HandleFunc("PATCH /api/v1/subscribes/{id}", rest.Update)
+	mux.HandleFunc("GET /api/v1/subscribes/{id}", rest.GetById)
+	mux.HandleFunc("GET /api/v1/subscribe", rest.GetList)
+	mux.HandleFunc("PUT /api/v1/subscribes/{id}", rest.UpdatePut)
+	mux.HandleFunc("PATCH /api/v1/subscribes/{id}", rest.UpdatePatch)
 	mux.HandleFunc("DELETE /api/v1/subscribes/{id}", rest.Delete)
 	mux.HandleFunc("*", func(w http.ResponseWriter, r *http.Request) {
 		errDto := models.NewFullExceptionDto(
@@ -133,7 +64,7 @@ func main() {
 
 	s := &http.Server{
 		Handler: rest.LoggingMiddleware(mux),
-		Addr:    serverAddr,
+		Addr:    serverAddrs,
 	}
 
 	go func() {
@@ -143,7 +74,7 @@ func main() {
 	}()
 
 	// shutting down server
-	log.Printf("Server starting on %s", serverAddr)
+	log.Printf("Server starting on %s", serverAddrs)
 	<-ctx.Done()
 
 	log.Println("Server shutting down...")
